fce09e3739df60bf36e027724a6c059b4aa2a9fe
juvsh <80842933+juvsh@users.noreply.github.com>
2021-06-11 11:10:14 +0300

    Delete src directory

--- src/views/About.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div class="About">
-  <p></p>
-</div>
-</template>
--- src/views/Media.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div class="media">
-  <iframe width="1280" height="720" src="https://www.youtube.com/embed/KB79BbwHp6Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-</div>
-</template>
